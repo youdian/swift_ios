788362580d4dee02bffdc32ba5732e80fd42df40
zhouzhou <zhouzhou@192.168.2.106>
2015-08-01 14:05:28 +0800

    use demo as the main entry

--- Demos/ViewController.swift
+++ Demos/ViewController.swift
@@ -9,53 +9,17 @@
 import UIKit
 import Foundation
 
-class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+class ViewController: UIViewController {
     var data = [String]()
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.dataSource = self
-        tableView.delegate = self
-        addElements(&data)
     }
-    func addElements(inout data:[String]) {
-        let count = 10;
-//        for i in 1...10 {
-//            data.append("string \(i)")
-//        }
-        for var i = 0; i < count; i++ {
-            data.append("string \(i)")
-        }
-    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 50
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let identifier = "NormalCell"
-        var cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath) as UITableViewCell
-        let position = indexPath.row
-        cell.textLabel?.text=data[position]
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        tableView.deselectRowAtIndexPath(indexPath, animated: false)
-        println("you select row \(indexPath.row)")
-    }
     
     
     
