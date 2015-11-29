9f2429af70ca640f97badea31042d94a1ea972b8
youdian <android@youdian.org>
2015-11-29 19:27:44 +0800

    update

--- tableviewdemo/tableviewdemo/PullToRefreshHeaderView.swift
+++ tableviewdemo/tableviewdemo/PullToRefreshHeaderView.swift
@@ -14,21 +14,31 @@ enum RefreshState: Int {
 
 class PullToRefreshHeaderView: UIView {
     var stateLabel: UILabel!
+    var activityIndicator: UIActivityIndicatorView!
     let offsetThreshold: CGFloat = 60
     let pullToRefeshText = "下拉刷新"
     let releaseToRefreshText = "松开刷新"
     private let loadingText = "正在刷新"
     let duration = 0.5
+    let indicatorSize: CGFloat = 15
     var state: RefreshState = .Normal {
         didSet {
             print("refreshView status changed to \(state)")
             switch state {
             case .Normal:
                 stateLabel.text = pullToRefeshText
+                activityIndicator.hidden = true
+                if activityIndicator.isAnimating() {
+                    activityIndicator.stopAnimating()
+                }
             case .Pulling:
                 stateLabel.text = releaseToRefreshText
+                if activityIndicator.hidden {
+                    activityIndicator.hidden = false
+                }
             case .Loading:
                 stateLabel.text = loadingText
+                activityIndicator.startAnimating()
                 
             }
         }
@@ -43,13 +53,32 @@ class PullToRefreshHeaderView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.autoresizingMask = .FlexibleWidth
-        self.backgroundColor = UIColor.greenColor()
-        stateLabel = UILabel(frame: CGRectMake(0, height - 40, width, 20))
-        stateLabel.autoresizingMask = .FlexibleWidth
+        self.backgroundColor = UIColor.clearColor()
+        stateLabel = UILabel()
+        stateLabel.translatesAutoresizingMaskIntoConstraints = false
         stateLabel.font = UIFont.systemFontOfSize(12)
-        stateLabel.textColor = UIColor.blueColor()
-        stateLabel.textAlignment = .Center
+        stateLabel.textColor = UIColor.grayColor()
         addSubview(stateLabel)
+        activityIndicator = UIActivityIndicatorView()
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.hidden = true
+        activityIndicator.color = UIColor.grayColor()
+        addSubview(activityIndicator)
+        let centerConstraint = NSLayoutConstraint(item: stateLabel, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1, constant: 0)
+        self.addConstraint(centerConstraint)
+        let yConstraint = NSLayoutConstraint(item: stateLabel, attribute: .Bottom, relatedBy: .Equal, toItem: self, attribute: .Bottom, multiplier: 1, constant: -20)
+        self.addConstraint(yConstraint)
+        let heightConstraint = NSLayoutConstraint(item: stateLabel, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 20)
+        stateLabel.addConstraint(heightConstraint)
+        
+        let indicatorWidthCons = NSLayoutConstraint(item: activityIndicator, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: indicatorSize)
+        activityIndicator.addConstraint(indicatorWidthCons)
+        let indicatorHeightCons = NSLayoutConstraint(item: activityIndicator, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: indicatorSize)
+        activityIndicator.addConstraint(indicatorHeightCons)
+        let indicatorXCons = NSLayoutConstraint(item: activityIndicator, attribute: .Right, relatedBy: .Equal, toItem: stateLabel, attribute: .Left, multiplier: 1, constant: -10)
+        self.addConstraint(indicatorXCons)
+        let indicatorYCons = NSLayoutConstraint(item: activityIndicator, attribute: .CenterY, relatedBy: .Equal, toItem: stateLabel, attribute: .CenterY, multiplier: 1, constant: 0)
+        self.addConstraint(indicatorYCons)
         state = .Normal
         stateLabel.text = pullToRefeshText
     }
